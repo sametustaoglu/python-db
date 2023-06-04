b6423671886d8f92c3c86befa764a0fd6f202583
samet-ustaoglu-wandai <samet.ustaoglu@wand.ai>
2023-06-04 08:20:15 +0300

    ci

---
+++ main.py
@@ -0,0 +1,31 @@
+# app.py
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+# PostgreSQL veritabanı bağlantısı için yapılandırma
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://username:password@host:port/database_name'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+
+    def __init__(self, name):
+        self.name = name
+
+@app.route('/')
+def hello():
+    return 'Hello, World!'
+
+@app.route('/users')
+def get_users():
+    users = User.query.all()
+    result = []
+    for user in users:
+        result.append(user.name)
+    return ', '.join(result)
+
+if __name__ == '__main__':
+    app.run()
